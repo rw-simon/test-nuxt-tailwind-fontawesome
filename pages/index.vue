56aef9934d9aff061ada2a4696f3c53e25b2e01a
rw-simon <simon.fischer@rechtwinklig.ch>
2022-01-25 09:50:17 +0100

    initial

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <icon class="fa-lg" :icon="`fal fa-sun`" />
+    <icon class="fa-lg" :icon="`fal fa-moon`" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
